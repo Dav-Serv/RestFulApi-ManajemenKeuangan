6db9d9c7a27c6a6566c14c38165b53508c0fd19f
davserv <dav.1234rp@gmail.com>
2026-08-29 00:40:40 +0700

    utils/response.go

--- internal/handlers/transaction_handler.go
+++ internal/handlers/transaction_handler.go
@@ -1,1 +1,42 @@
 package handlers
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"RestFulApi-ManajemenKeuangan/internal/database"
+	"RestFulApi-ManajemenKeuangan/internal/models"
+	"RestFulApi-ManajemenKeuangan/internal/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+// CreateTransaction - POST /api/transactions
+func CreateTransaction(c *gin.Context) {
+	userID := c.GetUint("user_id")
+
+	var input models.TransactionInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		utils.Error(c, http.StatusBadRequest, "input tidak valid:" + err.Error())
+		return
+	}
+
+	date := time.Now()
+	if input.Date != nil {
+		date = *input.Date
+	}
+
+	trx := models.Transaction{
+		UserID: 		userID,
+		Type: 			input.Type,
+		Category: 		input.Category,
+		Amount: 		input.Amount,
+		Description: 	input.Description,
+		Date: 			date,
+	}
+
+	if err := database.DB.Create(&trx).Error; err != nil {
+		utils.Error()
+	}
+}
--- internal/utils/response.go
+++ internal/utils/response.go
@@ -1,1 +1,20 @@
 package utils
+
+import "github.com/gin-gonic/gin"
+
+// success mengirim response suskses dengan format seragam.
+func Success(c *gin.Context, status int, message string, data interface{}) {
+	c.JSON(status, gin.H{
+		"success": 	true,
+		"message": 	message,
+		"data":		data,
+	})
+}
+
+// Error mengirim response error dengan format seragam.
+func Error(c *gin.Context, status int, message string) {
+	c.JSON(status, gin.H{
+		"success": false,
+		"message": message,
+	})
+}
